323325538b4a704d93ebbf1719bd4f9c1b978259
Max Kohn <mkohn@mkohn-ltm4.internal.salesforce.com>
2022-02-15 19:46:11 -0500

    added contact form and state changes for it

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,21 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
+
 
 function ContactForm() {
+
+    const [formState, setFormState] = useState({name: '', email: '', message: ''});
+    const {name, email, message} = formState;
+
+    /*function handleChange(e) {
+        setFormState({...formState, name: e.target.value});
+    }
+    console.log(formState);*/
+
+    function handleChange(e) {
+        //handle changes for all input fields and update one at a time based on name attribute on html
+        setFormState({...formState, [e.target.name]: e.target.value})
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState);
+    }
+
     return(
         <section>
             <h1>Contact Me</h1>
-            <form id="contact-form">
+            <form id="contact-form" onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="name">Name: </label>
-                    <input type="text" name="name"></input>
+                    <input type="text" name="name" defaultValue={name} onChange={handleChange}></input>
                 </div>
                 <div>
                     <label htmlFor="email">Email: </label>
-                    <input type="text" name="email"></input>
+                    <input type="text" name="email" defaultValue={email} onChange={handleChange}></input>
                 </div>
                 <div>
                     <label htmlFor="message">Message: </label>
-                    <textarea name="message"></textarea>
+                    <textarea name="message" defaultValue={message} onChange={handleChange}></textarea>
                 </div>
                 <button type="submit">Submit</button>
             </form>
